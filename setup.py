5f60dfed5892d36067eff92bbb123da4508d9205
Alexander Bohn / FI$H2k <fish2000@gmail.com>
2014-06-09 12:02:00 -0700

    Got rid of apostrophe quote-mark’s backslash

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 setup(
     name='urlstring',
     version='2.4.0',
-    description='A fork of Zachary Voase\'s "URLObject" utility class for manipulating URLs.',
+    description='''A fork of Zachary Voase’s “URLObject” utility class for manipulating URLs.''',
     author='Alexander Bohn',
     author_email='fish2000@gmail.com',
     url='http://github.com/fish2000/urlobject',
